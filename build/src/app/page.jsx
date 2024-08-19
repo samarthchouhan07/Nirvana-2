2057ff834e3f6b7a630c1fa328589e63764086f1
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-08-20 01:33:47 +0530

    Fix TypeScript errors

--- build/src/app/page.jsx
+++ build/src/app/page.jsx
@@ -3,6 +3,7 @@ import Hero from "@/components/hero/Hero";
 import PopularLocations from "@/components/popular-locations/PopularLocations";
 import sea from "../../public/sea.jpg";
 import hotel_image from "../../public/hr_10.jpg";
+export const dynamic = "force-dynamic";
 export default function Home() {
     return (<>
       <Hero image={sea} mainHeader="Are you ready for an adventure?" secondaryHeader="Browse through the popular locations"/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/hero/Hero";
 import PopularLocations from "@/components/popular-locations/PopularLocations";
 import sea from "../../public/sea.jpg"
 import hotel_image from "../../public/hr_10.jpg"
-
+export const dynamic = "force-dynamic";
 export default function Home() {
   return (
     <>
