61b194fe7db5c23f04c8408fc70354456b9c6799
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-08-21 13:45:24 +0530

    updated folder structure for the nextauth

--- src/lib/currentUser.ts
+++ src/lib/currentUser.ts
@@ -1,6 +1,6 @@
 import db from "./db";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { Session } from "next-auth";
 
 export const runtime = 'nodejs'
